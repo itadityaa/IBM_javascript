975fe810b347dbf3bbab537d01c05e0aa6760d99
itadityaa <it.adityaa@gmail.com>
2024-05-14 16:49:23 -0600

    Add code for weather report lab- Week 3

---
+++ Course1-Week3/FetchWeatherData/weather_report.js
@@ -0,0 +1,28 @@
+// d97ed6798e3d13ed6c5a05050e4fef34;
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+function showWeatherDetails(event) {
+  event.preventDefault();
+
+  const city = document.getElementById("city").value;
+  const apiKey = "d97ed6798e3d13ed6c5a05050e4fef34";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const weatherInfo = document.getElementById("weatherInfo");
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+      <p>Temperature: ${data.main.temp} &#8451</p>
+      <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      const weatherInfo = document.getElementById("weatherInfo");
+      weatherInfo.innerHTML = `<p>Failed to retrieve weather data. Please check city name.</p>`;
+    });
+}
+
+document
+  .getElementById("weatherForm")
+  .addEventListener("submit", showWeatherDetails);
