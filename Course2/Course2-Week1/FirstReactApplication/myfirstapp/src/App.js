a0b7b824793a3342ed362dbf78f2cf10cc0594a1
itadityaa <it.adityaa@gmail.com>
2024-06-07 16:11:17 -0600

    Add code for FirstReactApp lab- C2W1

--- Course2/Course2-Week1/FirstReactApplication/myfirstapp/src/App.js
+++ Course2/Course2-Week1/FirstReactApplication/myfirstapp/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props) {
+  const currDate = new Date();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, Aditya!</h1>
+      <p>Current Date: {currDate.toLocaleDateString()}</p>
+      <p>Current Time: {currDate.toLocaleTimeString()}</p>
+
+      <p>
+        Date: {currDate.toLocaleDateString()}, and the time is now:{" "}
+        {currDate.toLocaleTimeString()}
+      </p>
     </div>
   );
 }
